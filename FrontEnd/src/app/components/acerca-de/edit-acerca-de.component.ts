b7a3c89d6572e1ef15d4a1f362fe790d6dd6a705
Sofiaodello <101758267+Sofiaodello@users.noreply.github.com>
2022-10-29 16:21:38 -0300

    ajusto estructura de directorios

--- FrontEnd/src/app/components/acerca-de/edit-acerca-de.component.ts
+++ FrontEnd/src/app/components/acerca-de/edit-acerca-de.component.ts
@@ -44,8 +44,8 @@ export class EditAcercaDeComponent implements OnInit {
   }
 
   uploadImage($event: any){
-    //const id =  this.activatedRouter.snapshot.params['id'];
-    const name = "perfil_";
+    const id =  this.activatedRouter.snapshot.params['id'];
+    const name = "perfil_" + id;
     this.imageService.uploadImage($event, name)
 
   }
